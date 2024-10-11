911290725729b417b1b3a373212d12b387e00bec
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-11 13:06:11 +1000

    Add a program that generates random numbers for a number of lines

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,10 +1,17 @@
+"""
+CP1404/CP5632 Practical
+Generate 6 random numbers for each a number of lines based on user input
+"""
 import random
+
+MIN = 1
+MAX = 45
 NUMBER_OF_COLUMNS = 6
 number_of_quick_picks = int(input("How many quick picks? "))
 for i in range(number_of_quick_picks):
     random_numbers = []
     while len(random_numbers) < NUMBER_OF_COLUMNS:
-        random_number = random.randint(1, 45)
+        random_number = random.randint(MIN, MAX)
         if random_number not in random_numbers:
             random_numbers.append(random_number)
-    print(("{:2} "*NUMBER_OF_COLUMNS).format(*sorted(random_numbers)))
+    print(("{:2} " * NUMBER_OF_COLUMNS).format(*sorted(random_numbers)))
