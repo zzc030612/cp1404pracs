c572b84eda4621a161c5c896e245aa9c375a565e
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-18 13:39:50 +1000

    Refactor variable name and added a comment

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,19 +1,20 @@
 def main():
     """Print countries and championship winners."""
     player_to_win = {}
-    countries = []
+    winning_countries = []
     print("Wimbledon Champions:")
     with open("wimbledon.csv", "r", encoding="utf-8-sig") as in_file:
         contents = in_file.readlines()[1:]
-        process_contents_of_file(contents, countries, player_to_win)
+        process_contents_of_file(contents, winning_countries, player_to_win)
     print_championship_winners(player_to_win)
     print()
-    print_countries(countries)
+    print_countries(winning_countries)
 
 
 def process_contents_of_file(contents, countries, player_to_win):
     """Process the contents of a file into two variables."""
     for line in contents:
+        # line: 1968,AUS,Rod Laver,AUS,Tony Roche,"6–3, 6–4, 6–2\n"
         record = line.strip().split(",")
         player_to_win[record[2]] = player_to_win.get(record[2], 0) + 1
         countries.append(record[1])
@@ -27,8 +28,9 @@ def print_championship_winners(player_to_win):
 
 def print_countries(countries):
     """Print countries names that has won an championship."""
-    print(f"These {len(set(countries))} countries have won Wimbledon:")
-    print(", ".join(sorted(set(countries))))
+    unique_countries = set(countries)
+    print(f"These {len(unique_countries)} countries have won Wimbledon:")
+    print(", ".join(sorted(unique_countries)))
 
 
 main()
