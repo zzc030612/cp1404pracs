c8225df620df7d28ddcadac18d0dc9758feb71ac
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-18 11:38:59 +1000

    Add a loop that prints states and names neatly

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,7 +6,12 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-print(CODE_TO_NAME)
+
+max_name_length = max([len(name) for name in CODE_TO_NAME.values()])
+max_code_length = max([len(code) for code in CODE_TO_NAME.keys()])
+
+for code, name in CODE_TO_NAME.items():
+    print(f"{code:{max_code_length}} is {name}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
