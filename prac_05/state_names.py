903f1d87aa73ffcb2e40926a2a15fbe8053de93d
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-18 13:31:48 +1000

    Remove redundant decision structure

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -16,10 +16,7 @@ for code, name in CODE_TO_NAME.items():
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     try:
-        if state_code in CODE_TO_NAME:
-            print(state_code, "is", CODE_TO_NAME[state_code])
-        else:
-            raise ValueError
-    except ValueError:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    except KeyError:
         print(f"Invalid state: {state_code}")
     state_code = input("Enter short state: ").upper()
