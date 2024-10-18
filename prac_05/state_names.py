f499f4eeb0a0a8c52ef39a81ccaecd408863dd38
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-18 11:40:31 +1000

    Refactor the LBYL approach to use exceptions and the EAFP approach

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -2,6 +2,8 @@
 CP1404/CP5632 Practical
 State names in a dictionary
 File needs reformatting
+
+(delete) - good commit name - Complete prac 5 state names exercise
 """
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
@@ -15,8 +17,11 @@ for code, name in CODE_TO_NAME.items():
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
-        print("Invalid short state")
+    try:
+        if state_code in CODE_TO_NAME:
+            print(state_code, "is", CODE_TO_NAME[state_code])
+        else:
+            raise ValueError
+    except ValueError:
+        print(f"Invalid state: {state_code}")
     state_code = input("Enter short state: ").upper()
