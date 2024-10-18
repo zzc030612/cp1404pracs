7b5e7747ceed713271ed3f02ebf9174da31d03e0
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-18 11:45:27 +1000

    Fix naming issues

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -2,10 +2,9 @@
 
 string = input("Text: ")
 strings = string.split(" ")
-comparing_strings = set(strings)
 
-max_string_length = max(len(string) for string in comparing_strings)
-max_occurrence_length = max(len(str(strings.count(string))) for string in comparing_strings)
+max_string_length = max(len(string) for string in strings)
+max_occurrence_length = max(len(str(strings.count(string))) for string in strings)
 
-for string in sorted(comparing_strings):
+for string in sorted(set(strings)):
     print(f"{string:{max_string_length}} : {strings.count(string):{max_occurrence_length}}")
