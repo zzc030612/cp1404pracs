564bfacffff0acc3b052ad5f78e459fd400fdece
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-10-18 13:35:39 +1000

    Refactor variable name and added a comment

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,8 +1,10 @@
 name_to_email = {}
 email = input("Email: ")
 while email:
+    # Expected result:
+    # corey.camilleri@my.jcu.edu.au
     names = email.split("@")[0].split(".")
-    name = " ".join([item.title() for item in names])
+    name = " ".join([name.title() for name in names])
     choice = input(f"Is your name {name}? (Y/n) ")
     if choice.upper() == "N" or choice.upper() == "NO":
         name = input("Name: ")
