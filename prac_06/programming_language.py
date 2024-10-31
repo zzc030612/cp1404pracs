9a91384724120262c8e12ad0e217bffd4a1c3088
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-01 09:40:28 +1000

    Add an ProgrammingLanguage class

---
+++ prac_06/programming_language.py
@@ -0,0 +1,7 @@
+class ProgrammingLanguage:
+    def __init__(self, language="", typing="", reflection="", year=0):
+        self.language = language
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
