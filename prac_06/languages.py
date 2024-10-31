071f71b731ff5ae63ff29cff9165a61ba0b8498e
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-01 09:50:34 +1000

    Refactor class objects into lists

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,6 +1,7 @@
 from prac_06.programming_language import ProgrammingLanguage
 
-python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-print(python)
+programming_languages = [[ProgrammingLanguage("Python", "Dynamic", True, 1991)],
+                         [ProgrammingLanguage("Ruby", "Dynamic", True, 1995)],
+                         [ProgrammingLanguage("Visual Basic", "Static", False, 1991)]]
+
+print(programming_languages)
