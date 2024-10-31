18501f5dbc026cf961c13f77dd6f550c7a745b70
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-01 09:45:12 +1000

    Add values to test class

---
+++ prac_06/languages.py
@@ -0,0 +1,6 @@
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
