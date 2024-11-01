18951dfe508c27c07035fc19d311e2d79bfbb5d4
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-01 10:18:12 +1000

    Add docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,12 +1,20 @@
+"""
+Time started: 9:35
+Time finished:
+"""
+
 class ProgrammingLanguage:
     def __init__(self, language="", typing="", reflection="", year=0):
+        """Initialise variables"""
         self.language = language
-        self.typing = typing
+        self.typing = typing.lower()
         self.reflection = reflection
         self.year = year
 
-    def __str__(self):
-        return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in{self.year}"
+    def __repr__(self):
+        """Return a string representation of the object"""
+        return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
-        return self.language == "dynamic"
+        """Check if the language is dynamic"""
+        return self.typing == "dynamic"
