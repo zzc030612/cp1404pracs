85bce83f634e9ff9fcdee86be0234f1b1957e42e
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-02 11:14:48 +1000

    Complete programming language class exercise

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,20 +1,21 @@
 """
 Time started: 9:35
-Time finished:
+Time finished: 10:37
 """
+
 
 class ProgrammingLanguage:
     def __init__(self, language="", typing="", reflection="", year=0):
         """Initialise variables"""
         self.language = language
-        self.typing = typing.lower()
+        self.typing = typing
         self.reflection = reflection
         self.year = year
 
-    def __repr__(self):
+    def __str__(self):
         """Return a string representation of the object"""
         return f"{self.language}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
         """Check if the language is dynamic"""
-        return self.typing == "dynamic"
+        return self.typing == "Dynamic"
