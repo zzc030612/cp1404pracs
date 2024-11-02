3a54324f7998d3dc46e40c55ac0e217e18f93078
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-02 11:15:19 +1000

    Complete using a class, for the programming language exercise

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,7 +1,15 @@
+"""
+Time started: 9:35
+Time finished: 10:37
+"""
+
 from prac_06.programming_language import ProgrammingLanguage
 
-programming_languages = [[ProgrammingLanguage("Python", "Dynamic", True, 1991)],
-                         [ProgrammingLanguage("Ruby", "Dynamic", True, 1995)],
-                         [ProgrammingLanguage("Visual Basic", "Static", False, 1991)]]
+programming_languages = [ProgrammingLanguage("Python", "Dynamic", True, 1991),
+                         ProgrammingLanguage("Ruby", "Dynamic", True, 1995),
+                         ProgrammingLanguage("Visual Basic", "Static", False, 1991)]
 
-print(programming_languages)
+print("The dynamically typed languages are:")
+dynamic_languages = [programming_language.language for programming_language in programming_languages if
+                     programming_language.is_dynamic()]
+print("\n".join(dynamic_languages))
