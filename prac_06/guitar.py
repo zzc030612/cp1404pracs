3f5e579dbdba6b52814beae88281baa35e8e20dd
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-02 11:15:59 +1000

    Complete creating a class, for the guitar exercise

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,3 +3,12 @@ class Guitar:
         self.name = name
         self.year = year
         self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : ${self.cost}"
+
+    def get_age(self):
+        return 2024 - self.year
+
+    def is_vintage(self):
+        return self.get_age() > 50
