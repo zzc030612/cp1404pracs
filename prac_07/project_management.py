37311a26341ab90ca8db16c8913fd01ff517939f
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-08 15:43:17 +1000

    Add __init__ method to class

---
+++ prac_07/project_management.py
@@ -0,0 +1,14 @@
+"""
+Word Occurrences
+Estimate: 120 minutes
+Actual:   ?? minutes
+
+This time is also counting the time it taken to complete the other project file
+"""
+class ProjectManagement:
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
