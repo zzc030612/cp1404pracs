6a53a491864134bc9fc8b2a2e2c970ebac76e3b0
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-08 15:45:21 +1000

    Add file processing

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,10 +1,11 @@
 """
 Word Occurrences
-Estimate: 90 minutes
+Estimate: 120 minutes
 Actual:   ?? minutes
 
 This time is also counting the time it taken to complete the other project file
 """
+from prac_07.project_management import ProjectManagement
 
 MENU = """- (L)oad projects
 - (S)ave projects
@@ -14,11 +15,21 @@ MENU = """- (L)oad projects
 - (U)pdate project
 - (Q)uit"""
 
+filename = "project.py"
+projects = []
+
+with open(filename, "r") as in_file:
+    in_file.readline()
+    for line in in_file:
+        part = line.split(" ")
+        project = ProjectManagement(part[0], part[1], part[2], part[3], part[4])
+        projects.append(project)
+
 print(MENU)
 choice = input(">>> ").upper()
 while choice != "Q":
     if choice == "L":
-        pass
+        filename = input("Enter a file name: ")
     elif choice == "S":
         pass
     elif choice == "D":
