1e9357eff2c8485d8b3c927273706b1c4ab01f70
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-08 15:49:24 +1000

    Refactor to use functions

--- prac_07/project.py
+++ prac_07/project.py
@@ -15,33 +15,43 @@ MENU = """- (L)oad projects
 - (U)pdate project
 - (Q)uit"""
 
-filename = "project.py"
-projects = []
 
-with open(filename, "r") as in_file:
-    in_file.readline()
-    for line in in_file:
-        part = line.split(" ")
-        project = ProjectManagement(part[0], part[1], part[2], part[3], part[4])
-        projects.append(project)
-
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "L":
-        filename = input("Enter a file name: ")
-    elif choice == "S":
-        pass
-    elif choice == "D":
-        pass
-    elif choice == "F":
-        pass
-    elif choice == "A":
-        pass
-    elif choice == "U":
-        pass
-    else:
-        print("Error invalid input")
-
+def main():
+    filename = "project.py"
+    process_file(filename)
     print(MENU)
     choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            filename = input("Enter a file name: ")
+            projects = process_file(filename)
+            print(f"Loaded {len(projects)} projects from {filename}")
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Error invalid input")
+
+        print(MENU)
+        choice = input(">>> ").upper()
+
+
+def process_file(filename):
+    projects = []
+    with open(filename, "r") as in_file:
+        in_file.readline()
+        for line in in_file:
+            part = line.split(" ")
+            project = ProjectManagement(part[0], part[1], part[2], part[3], part[4])
+            projects.append(project)
+    return projects
+
+
+main()
