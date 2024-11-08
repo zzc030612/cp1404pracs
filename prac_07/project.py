2a71577003e8d5e1273a3efb06cfa1623a15072e
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-08 15:54:47 +1000

    Add save to file function to project.py

--- prac_07/project.py
+++ prac_07/project.py
@@ -19,15 +19,15 @@ MENU = """- (L)oad projects
 def main():
     filename = "project.py"
     process_file(filename)
+    projects = process_file(filename)
     print(MENU)
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "L":
             filename = input("Enter a file name: ")
             projects = process_file(filename)
-            print(f"Loaded {len(projects)} projects from {filename}")
         elif choice == "S":
-            pass
+            save_to_file(projects, filename)
         elif choice == "D":
             pass
         elif choice == "F":
@@ -51,7 +51,15 @@ def process_file(filename):
             part = line.split(" ")
             project = ProjectManagement(part[0], part[1], part[2], part[3], part[4])
             projects.append(project)
+    print(f"Loaded {len(projects)} projects from {filename}")
     return projects
+
+
+def save_to_file(projects, filename):
+    with open(filename, "w") as out_file:
+        for project in projects:
+            print(f"{project.name}  {project.start_date}    {project.priority}    {project.cost_estimate}   "
+                  f"{project.completion_percentage}", file=out_file)
 
 
 main()
