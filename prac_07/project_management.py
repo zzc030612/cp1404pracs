f555095c89384bfdbe021269b0c67560bd1faa18
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-10 23:09:11 +1000

    Complete the project management class prac exercise

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,10 +5,25 @@ Actual:   ?? minutes
 
 This time is also counting the time it taken to complete the other project file
 """
+import datetime
+
+
 class ProjectManagement:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
         self.start_date = start_date
-        self.priority = priority
+        self.priority = int(priority)
         self.cost_estimate = cost_estimate
-        self.completion_percentage = completion_percentage
+        self.completion_percentage = int(completion_percentage)
+
+        self.converted_date = datetime.datetime.strptime(self.start_date, "%d/%m/%Y").date()
+
+
+    def __str__(self):
+        return f"{self.name}, start: {self.start_date}, priority: {self.priority}, estimate: {self.cost_estimate}, completion: {self.completion_percentage} %"
+
+    def __lt__(self, other):
+        return self.priority < other.priority
+
+    def is_complete(self):
+        return self.completion_percentage == 100
