d40d8c9b74e25bcc5aae27a66d3f6d57bdd97fac
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-21 22:28:08 +1000

    Add 'get taxi' function to taxi simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,16 +1,36 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+from prac_09.taxi import Taxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
-    choice = input(MENU)
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print(MENU)
+    choice = input(">>> ")
     while choice != "q":
         if choice == "c":
-            pass
+            current_taxi = get_taxi(taxis)
         elif choice == "d":
             pass
         else:
             print("Invalid option")
         print("Bill to date: ")
-        choice = input(MENU)
+        print(MENU)
+        choice = input(">>> ")
+
+
+def get_taxi(taxis):
+    for i, taxi in enumerate(taxis):
+        print(f"{i} - {taxi}")
+    try:
+        taxi_index = int(input(">>> "))
+        if  0 <= taxi_index < len(taxis):
+            current_taxi = taxis[taxi_index]
+            return current_taxi
+        print("Invalid taxi choice")
+    except ValueError:
+        print("Invalid taxi choice")
+
+
 
 main()
