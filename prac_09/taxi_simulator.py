c35d37ced30ae0f87da193c97f7d03adc08ac8a6
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-21 23:03:39 +1000

    Complete the 'd' menu option in taxi simulator

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -1,3 +1,5 @@
+from types import NoneType
+
 from prac_09.silver_service_taxi import SilverServiceTaxi
 from prac_09.taxi import Taxi
 
@@ -5,21 +7,36 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    current_taxi = None
+    total_cost = 0
     print(MENU)
     choice = input(">>> ")
     while choice != "q":
         if choice == "c":
-            current_taxi = get_taxi(taxis)
+            current_taxi = get_taxi(taxis, current_taxi)
         elif choice == "d":
-            pass
+            if current_taxi:
+                if current_taxi.fuel != 0:
+                    try:
+                        current_taxi.start_fare()
+                        distance_driven = int(input("Drive how far? "))
+                        current_taxi.drive(distance_driven)
+                        total_cost += current_taxi.get_fare()
+                        print(f"{current_taxi.name} trip cost you ${total_cost}")
+                    except ValueError:
+                        print("Invalid input")
+                else:
+                    print(f"The {current_taxi.name} has no fuel.")
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
-        print("Bill to date: ")
+        print(f"Bill to date: {total_cost}")
         print(MENU)
         choice = input(">>> ")
 
 
-def get_taxi(taxis):
+def get_taxi(taxis, current_taxi):
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
     try:
@@ -27,9 +44,11 @@ def get_taxi(taxis):
         if  0 <= taxi_index < len(taxis):
             current_taxi = taxis[taxi_index]
             return current_taxi
-        print("Invalid taxi choice")
     except ValueError:
-        print("Invalid taxi choice")
+        pass
+    print("Invalid taxi choice")
+    return current_taxi
+
 
 
 
