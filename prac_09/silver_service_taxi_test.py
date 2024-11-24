45dd5708c9ba9d67dc59c891f4eb59f6f1f07462
Corey-Camilleri <corey.camilleri@my.jcu.edu.au>
2024-11-24 16:38:04 +1000

    Complete the testing file for silver service taxi prac exercise

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+my_silver_service_taxi = SilverServiceTaxi("Hummer", 200, 2)
+my_silver_service_taxi.drive(18)
+print(my_silver_service_taxi)
+print(my_silver_service_taxi.get_fare())
