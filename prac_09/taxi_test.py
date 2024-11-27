de205f35aa691b214f499e3471e07e5aedee9bad
zzc030612 <151332431+zzc030612@users.noreply.github.com>
2024-11-27 18:36:07 +0800

    Update taxi_test.py

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,10 +1,13 @@
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius 1", 100)
-my_taxi.drive(40)
-print(my_taxi)
-print(my_taxi.get_fare())
-my_taxi.start_fare()
-my_taxi.drive(100)
-print(my_taxi)
-print(my_taxi.get_fare())
+def main():
+  my_taxi = Taxi("Prius 1", 100)
+  my_taxi.drive(40)
+  print(my_taxi)
+  print(my_taxi.get_fare())
+  my_taxi.start_fare()
+  my_taxi.drive(100)
+  print(my_taxi)
+  print(my_taxi.get_fare())
+
+main()
